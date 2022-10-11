5db714a2667905ad974023b31a68ea0b64c2dc8d
LPastine <lucianopastine.tech@gmail.com>
2022-10-11 07:00:11 -0300

    variables

--- variables.py
+++ variables.py
@@ -10,6 +10,30 @@ can be single or double quotes
 """
 VARIABLE RULES:
   - Variable names are case sensitive (name and NAME are different variables)
-  - Must start with a letter or an underscore
+  - Must start with a letter or an underscorey
   - Can have numbers but can not start with one
 """
+# x = 1
+# y = 2.5
+# name = 'Brad'
+# isCool = True
+
+# Multiple assignments
+x, y, name, isCool = (1, 2.5, 'Brad', True)
+
+print(x, y, name, isCool)
+
+# Basic Math
+a = x + y
+
+print(a)
+
+# Check Type
+print(type(x))
+
+# Casting
+x = str(x)
+y = int(y)
+
+print(type(x))
+print(type(y))
