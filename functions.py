fc1f455753adaed1cb3f7d295623d054b70d2182
LPastine <lucianopastine.tech@gmail.com>
2022-10-11 09:55:18 -0300

    functions

--- functions.py
+++ functions.py
@@ -1,7 +1,46 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
+# Create function
+def sayHello(name='Sam'):
+    """
+    Prints Hello and then name.
+    """
+    print('Hello ' + name)
+
+
+sayHello('Sam')
+
+# Return value
+
+
+# def getSum(num1, num2):
+#     total = num1 + num2
+#     return total
+
+
+# numSum = getSum(2, 3)
+
+
+# def addOneToNum(num):
+#     num += 1
+#     return num
+
+
+# num = 5
+# new_num = addOneToNum(num)
 
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
+# Lambda functions were converted to inline normal functions by Pylance
 
+def getSum(num1, num2): return num1 + num2
+
+
+print(getSum(9, 2))
+
+
+def addOneToNum(num): return num + 1
+
+
+print(addOneToNum(5))
